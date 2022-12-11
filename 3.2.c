b6fbc33435f38ec1c18433496744f948c5348da2
Rory Bradford <roryrjb@gmail.com>
2022-12-11 15:08:34 +0000

    day 3 part 2

---
+++ 3.2.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Lowercase item types a through z have priorities 1 through 26. ('a' == 97)
+// Uppercase item types A through Z have priorities 27 through 52. ('A' == 65)
+
+#define BUF_SIZE 64
+
+int main()
+{
+    char data[BUF_SIZE];
+    FILE *input = fopen("3.txt", "r");
+    int total = 0;
+
+    if (input == NULL)
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    int counter = 0;
+    char group[3][BUF_SIZE];
+    char match = '\0';
+
+    while (fgets(data, BUF_SIZE, input) != NULL)
+    {
+        strcpy(group[counter++], data);
+
+        if (counter == 3)
+        {
+            counter = 0;
+            char *a = group[0];
+            char *b = group[1];
+            char *c = group[2];
+
+            for (int i = 0; i < strlen(a); i++)
+            {
+                for (int j = 0; j < strlen(b); j++)
+                {
+                    for (int k = 0; k < strlen(c); k++)
+                    {
+                        if (a[i] == b[j] && a[i] == c[k] && c[k] == b[j])
+                        {
+                            match = a[i];
+                            break;
+                        }
+                    }
+
+                    if (match != '\0')
+                        break;
+                }
+
+                if (match != '\0')
+                    break;
+            }
+
+            int score = (match >= 97) ? match - 96 : match - 38;
+            match = '\0';
+            total += score;
+        }
+    }
+
+    printf("3.2: %d\n", total);
+}
